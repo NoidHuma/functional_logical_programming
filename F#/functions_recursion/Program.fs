866c0337efed47b76c550f9a2ae0b1460ba13e39
NoidHuma <nikitag.big2005@gmail.com>
2025-03-28 23:49:57 +0300

    lab 5 2 quadratic equation

--- F#/functions_recursion/Program.fs
+++ F#/functions_recursion/Program.fs
@@ -1,1 +1,29 @@
-﻿printfn "Hello from F#, World!"
+﻿open System
+
+
+type SolveResult =
+    None
+    | Linear of float
+    | Quadratic of float*float
+
+
+let solve a b c = 
+    let D = b * b - 4. * a * c
+    if a = 0. then
+        if b = 0. then None
+        else Linear(-c / b)
+    else
+        if D < 0 then None
+        else Quadratic(((-b + sqrt(D)) / (2. * a), (-b - sqrt(D)) / (2. * a)))
+
+
+[<EntryPoint>]
+let main argv = 
+    let res = solve 4 -24 36
+    match res with
+         None -> Console.WriteLine("Нет решений")
+         | Linear(x) -> Console.WriteLine($"Линейное уравнение, корень: {x}.")
+         | Quadratic(x1, x2) when x1 = x2 -> Console.WriteLine($"Квадратичное уравнение, 1 корень: {x1}.")
+         | Quadratic(x1, x2) -> Console.WriteLine($"Квадратичное уравнение, 2 корня: {x1}, {x2}.")
+
+    0
